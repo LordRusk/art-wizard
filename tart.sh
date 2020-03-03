bd3532b2fd706416801c612c6ebc24a94d9dbfff
LordRusk <54194338+LordRusk@users.noreply.github.com>
2020-03-03 04:52:53 +0000

    Add files via upload

--- tart.sh
+++ tart.sh
@@ -7,22 +7,15 @@ echo "This script makes it easy and fast to install Arch or Artix while meeting 
 echo "Press Enter to continue..."
 read temp
 
-clear
-echo "Tart needs dash, tcc, make, patch, and umenu. Would you like to install them? Y/n"
-read temp
-case $temp in
-	n) echo "Exiting INstaller..."; exit ;;
-	*)
-	pacman --noconfirm --needed -Sy dash tcc make patch;
-	git clone https://www.github.com/LordRusk/tart
-	git clone https://github.com/JCoMcL/umenu
-	cp tart/umenu-tcc.diff umenu/
-	cd umenu
-	patch < umenu-tcc.diff
-	make install
-	cd ../
-	cd tart
-	dash tart-pre-chroot
-	;;
-esac
+# Install Needed Programs and Packages
+pacman --noconfirm --needed -Sy dash tcc make patch;
+git clone https://www.github.com/LordRusk/tart
+git clone https://github.com/JCoMcL/umenu
+cp tart/umenu-tcc.diff umenu/
+cd umenu
+patch < umenu-tcc.diff
+make install
+cd ../
+cd tart
+dash tart-pre-chroot
 
