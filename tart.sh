a98cab1a9f8f032ab1b8fe49c3062eef87ae720e
Rusk <lordruskk@gmail.com>
2020-04-09 11:30:58 -0700

    continuein the quality rewrite

--- tart.sh
+++ tart.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
 
-# Welcome
-clear
-echo "Welcome to the Tiny Art Installer"
-echo "This script makes it fast and easy and fast to install Arch or Artix while meeting many needs you may have."
-echo "Any programs that are needed and installed by the script that are not needed afterwords will be removed from your install"
-echo "Press Enter to continue..."
-read temp
+cat << EOF
+Welcome to The Art Wizard,
+--------------------------
+This wzard is here to help
+you install Arch or Artix
+fast, easy, and with as
+much customazation as
+possible.
+---------------------------
+Please contribute by adding
+more bootloader support,
+better writen code, etc.
+---------------------------
+Would you like to continue
+With the installer? [Y/n]
+EOF
 
-# Install Needed Programs and Packages
-pacman --noconfirm --needed -Sy dash tcc make patch;
-git clone https://www.github.com/LordRusk/tart
-git clone https://github.com/JCoMcL/umenu
-cp tart/umenu-tcc.diff umenu/
-cd umenu
-patch < umenu-tcc.diff
-make install
-cd ../
-dash tart/tart-pre-chroot
+read -r tmp
+case $tmp in
+	[Nn]) break;;
+	*) sh ~/tart/bin/tart-pre-chroot ;;
+esac
 
