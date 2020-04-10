6394a4c9170ac2845a4e4039de444f767bc8ace7
rusk <lordruskk@gmail.com>
2020-04-10 01:25:07 +0000

    coninueing quality testing

--- tart.sh
+++ tart.sh
@@ -8,32 +8,22 @@ esac done
 clear
 cat << EOF
 Welcome to The Art Wizard,
---------------------------
-This wizard is here to help
-you install Arch or Artix
-fast, easy, and with as
-much customazation as
-possible.
----------------------------
-This wizard uses the core
-of LARBS to install specific
-programs defined in
-tart/share/progs.csv, in
-this file, you can find
-commented out examples for
-xorg. If exit the wizard
-and edit the file.
----------------------------
-This wizard also has the
-capability of depoying
-a dotfiles repository. This
-can be achieved with option
--d [git url].
----------------------------
-Please contribute by adding
-more bootloader support,
+------------------------------------------------------
+This wizard is here to help you install Arch or Artix
+fast, easy, and with as much customazation as possible.
+------------------------------------------------------
+This wizard uses the core of LARBS to install specific
+programs defined in tart/share/progs.csv, in this file,
+you can find commented out examples for xorg. If exit
+the wizard and edit the file.
+------------------------------------------------------
+This wizard also has the capability of depoying a
+dotfiles repository. This can be achieved with option
+d [git url].
+------------------------------------------------------
+Please contribute by adding more bootloader support,
 better writen code, etc.
----------------------------
+------------------------------------------------------
 Would you like to continue
 With the installer? [Y/n]
 EOF
