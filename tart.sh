9e19833cb970af69c51834c524bbf03295fc585d
rusk <lordruskk@gmail.com>
2020-04-10 00:50:47 +0000

    coninueing quality testing

--- tart.sh
+++ tart.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
+while getopts ":b:" o; do case "${o}" in
+	d) dotfilesrepo=${OPTARG} && git ls-remote "$dotfilesrepo" || exit ;;
+	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit ;;
+esac done
+
 clear
 cat << EOF
 Welcome to The Art Wizard,
@@ -10,6 +15,21 @@ fast, easy, and with as
 much customazation as
 possible.
 ---------------------------
+This wizard uses the core
+of LARBS to install specific
+programs defined in
+tart/share/progs.csv, in
+this file, you can find
+commented out examples for
+xorg. If exit the wizard
+and edit the file.
+---------------------------
+This wizard also has the
+capability of depoying
+a dotfiles repository. This
+can be achieved with option
+-d [git url].
+---------------------------
 Please contribute by adding
 more bootloader support,
 better writen code, etc.
@@ -17,7 +37,6 @@ better writen code, etc.
 Would you like to continue
 With the installer? [Y/n]
 EOF
-
 read -r tmp
 case $tmp in
 	[Nn]) break;;
@@ -25,7 +44,7 @@ case $tmp in
 	printf "Downloading Art Wizard Repository..."
 	cd ~/
 	git clone https://www.github.com/lordrusk/tart >/dev/null 2>&1
+	export dotfilesrepo
 	sh ~/tart/bin/tart-pre-chroot
 	;;
 esac
-
