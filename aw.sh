ab2ce5030a6bdfcb2cbb1d0cd7e714602af0f70f
rusk <lordruskk@gmail.com>
2020-04-12 17:09:33 +0000

    Continueing addeding and testing features in vm

--- aw.sh
+++ aw.sh
@@ -13,18 +13,18 @@ This wizard is here to help you install Arch or Artix
 fast, easy, and with as much customazation as possible.
 ------------------------------------------------------
 This wizard uses the core of LARBS to install specific
-programs defined in tart/share/progs.csv, in this file,
-you can find commented out examples for xorg. If
-needed, exit the wizard and edit the file.
+programs defined in art-wizard/share/progs.csv, in
+this file, you can find commented out examples for
+xorg. If needed, exit the wizard and edit the file.
 ------------------------------------------------------
 This wizard also has the capability of depoying a
 dotfiles repository. This can be achieved with option
 -d [git url]. You do need to create the user for this
 operation, so if you skip that step, even if you use
--d, the dotfiles will not be release.
+-d, the dotfiles will not be released.
 ------------------------------------------------------
 Please contribute by adding more bootloader support,
-better writen code, etc.
+more network managers, better writen code, etc.
 ------------------------------------------------------
 Would you like to continue
 With the installer? [Y/n]
